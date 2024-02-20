6fc38f7da1e0e00bcdd40064582224f79e476614
Pangambam <53959992+Pangambam@users.noreply.github.com>
2024-02-20 13:55:41 +0530

    Update Factorial.c

--- src/c/Factorial.c
+++ src/c/Factorial.c
@@ -5,21 +5,22 @@
 *	Exemplos de função para retornar o fatorial de um número n
 *	função recursiva
 */
+long long factorial(int);
 
 int main(){
     int num;
     printf("Digite um número: ");
     scanf("%d", &num);
-    int result = fatorial(num);
+    long long result = factorial(num);
     printf("1 => ");
-    printf("%d! é : %d\n", num, result);
+    printf("%d! é : %lld\n", num, result);
     return(0);
 }
 
-int fatorial(int num){
+long long factorial(int num){
     if(num <= 1 ){
         return 1;
     }
     printf("%d * ", num);
-    return num * fatorial(num - 1);
+    return (long long)num * factorial(num - 1);
 }
